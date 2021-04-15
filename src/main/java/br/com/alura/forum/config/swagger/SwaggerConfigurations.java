06c1802fd4f0559ccd48dc57b3c0fc2830316cc1
Magno Azevedo <magnoazneto@gmail.com>
2021-04-15 19:22:58 -0300

    adiciona autenticacao via token com um header para o swagger

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -3,10 +3,14 @@ package br.com.alura.forum.config.swagger;
 import br.com.alura.forum.usuario.Usuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfigurations {
@@ -18,7 +22,14 @@ public class SwaggerConfigurations {
                 .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(Usuario.class);
-
+                .ignoredParameterTypes(Usuario.class)
+                .globalOperationParameters(Arrays.asList(
+                        new ParameterBuilder()
+                        .name("Authorization")
+                        .description("Header para token JWT")
+                        .modelRef(new ModelRef("string"))
+                        .parameterType("header")
+                        .required(false)
+                        .build()));
     }
 }
